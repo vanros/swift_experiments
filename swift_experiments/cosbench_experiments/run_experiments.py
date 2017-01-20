4499e11b7849a950b9e3b3b699476a9cd13ffa30
vanros <vanessarodrigues.vj@gmail.com>
2017-01-20 11:43:51 -0800

    Tentando implementar Fabric python

--- swift_experiments/cosbench_experiments/run_experiments.py
+++ swift_experiments/cosbench_experiments/run_experiments.py
@@ -9,67 +9,31 @@ import time
 import shutil
 import xml.etree.ElementTree as ET
 import select
+from fabric.api import env, run
+from fabric.api import task
+from fabric.tasks import Task
 
 SWIFT_CONF_DIR = "swift_conf"
 
-class SwiftNodeSSH:
-    ip = ""
-    ssh = ""
-    username = ""
-    password = ""
-
-    def __init__(self):
+class SwiftNodeSSH(Task):
+    ip = []
+    def __init__(self, ip):
         self.ip = ip
-        self.ssh = paramiko.SSHClient()
-        self.username = username
-        self.password = password
-        self.sudo_command = "echo " + self.password + " | sudo -S"
-
-    def connect(self):
-        try:
-            self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self.ssh.connect(self.ip,
-                         username=self.username,
-                         password=self.password)
-        except paramiko.AuthenticationException:
-            print("Authentication failed when connecting to %s" % self.ip)
-            exit(1)
-        except:
-            print("Could not SSH to %s" % self.ip)
-
-    def exec_ssh(self, *args, **kwargs):
-        command = kwargs.get("command", None)
-        if command:
-            print("Command: %s" % command),
-            stdin, stdout, stderr = self.ssh.exec_command(command)
-            while not stdout.channel.exit_status_ready():
-                if stdout.channel.recv_ready():
-                    rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
-                    if len(rl) > 0:
-                        print stdout.channel.recv(1024)
-            if stdout and stdout.channel.recv_exit_status() != 0:
-                print("stdout exit status: %s" %
-                      stdout.channel.recv_exit_status())
-                data_err = stderr.channel.recv_stderr(1024)
-                if data_err:
-                    print(data_err)
-                exit(1)
-        else:
-            print("Error no command requested")
-            exit(1)
+    #env.hosts = ['192.168.1.100', '192.168.1.101', '192.168.1.102']
+    for i in ip:
+        currenty_ip = ip[i]
+        env.hosts.append(currenty_ip)
 
     def source(self):
-        self.exec_ssh(command=self.sudo_command + "source /root/openrc")
+        run("source /root/openrc")
 
     def stop_all_swif_servers(self):
-        self.exec_ssh(command=self.sudo_command + " swift-init all stop")
+        run(" swift-init all stop")
 
     def erase_swift_data(self):
-        self.exec_ssh(command=self.sudo_command + " swift delete --all")
+        run(" swift delete --all")
 
-    def close(self):
-        if self.ssh:
-            self.ssh.close()
+
 
 class FilePathWrapper():
     file_name = ""
@@ -88,11 +52,11 @@ class COSBenchWorkload():
     executions_count = 0
     proxy_node_ssh = None
 
-    def __init__(self, cli_path, workload_path, ,
+    def __init__(self, cli_path, workload_path, storage_policies,
                  executions_count, proxy_node_ssh):
         self.cli_path = cli_path
         self.workload_path = workload_path
-        self.sort_strategies = sort_strategies
+        self.storage_policies = storage_policies
         self.executions_count = executions_count
         self.proxy_node_ssh = proxy_node_ssh
 
@@ -103,6 +67,12 @@ class COSBenchWorkload():
                 file_path_wrapper = FilePathWrapper(f, self.workload_path + f)
                 filepaths.append(file_path_wrapper)
         self.filepaths = filepaths
+
+    def _reset_archives(self):
+        filename = raw_input('Delete old archives [y/n]: ')
+        if "y" in filename:
+            shutil.rmtree(self.cli_path + "archive", ignore_errors=True)
+            self._restart_cosbench()
 
     def _restart_cosbench(self):
         cur_dir = os.getcwd()
@@ -115,12 +85,23 @@ class COSBenchWorkload():
 
     def _update_workload_xml(self,
                              current_file_path,
-                             current_strategy):
+                             current_storage_policies):
         tree = ET.parse(current_file_path.file_path)
         root = tree.getroot()
         root.set('name', current_file_path.file_name + "-" +
-                 current_strategy.strip())
+                 current_storage_policies.strip())
         tree.write(current_file_path.file_path)
+
+    def run(self):
+        self._reset_archives()
+        self._get_workloads()
+        for filepath in self.filepaths:
+            for current in range(self.executions_count):
+                for storage_policy in self.storage_policies:
+                    SwiftNodeSSH.source()
+
+
+
 
 
 
